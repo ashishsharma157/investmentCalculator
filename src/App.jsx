0938cce880718d21974743bddc9ece161d4894a9
CodeSandbox <support@codesandbox.io>
2023-11-14 04:16:00 +0000

    Initial commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,34 @@
+import { useState } from "react";
+import Header from "./Components/Header";
+import UserInput from "./Components/UserInput";
+import Results from "./Components/Results";
 function App() {
+  const [userInput, setUsetInput] = useState({
+    initialInvestment: 10000,
+    annualInvestment: 1200,
+    expectedReturn: 6,
+    duration: 10,
+  });
+  function handleChange(inputIdentifier, newValue) {
+    setUsetInput((prevUserInput) => {
+      return {
+        ...prevUserInput,
+        [inputIdentifier]: +newValue,
+      };
+    });
+  }
+
+  const inputIsValid = userInput.duration >= 1;
   return (
-    <h1>React Investment Calculator</h1>
-  )
+    <>
+      <Header />
+      <UserInput onChangeInput={handleChange} userInput={userInput} />
+      {!inputIsValid && (
+        <p className="center">Please enter duration greater than zero.</p>
+      )}
+      {inputIsValid && <Results input={userInput} />}
+    </>
+  );
 }
 
-export default App
+export default App;
